ad4666212a7c426825f906dfa6bb8e2114761d44
eNV25 <61089994+eNV25@users.noreply.github.com>
2021-04-20 06:33:10 +0200

    export-youtube-playlist.js: update formatting

--- export-youtube-playlist.js
+++ export-youtube-playlist.js
@@ -1,4 +1,6 @@
 // run in the browser console after loading the full playlist
-var out = ""
-$$("a.ytd-playlist-video-renderer").forEach(item => out += "https://youtube.com" + item.getAttribute("href") + "\n")
-copy(out)
+var out = "";
+$$("a.ytd-playlist-video-renderer").forEach((item) => {
+    out += "https://youtube.com" + item.getAttribute("href") + "\n";
+});
+copy(out);
