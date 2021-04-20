e5a755b85c6400de78803f8e25e0f7a0afd244ae
eNV25 <61089994+eNV25@users.noreply.github.com>
2021-04-20 06:28:48 +0200

    Change print.py. Make print.v1.py.

--- print.py
+++ print.py
@@ -2,17 +2,26 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file")
+parser.add_argument(
+    "file",
+    nargs="?",  # argument is optional
+    default="/dev/stdin",
+)
 
 if __name__ == "__main__":
     with open(parser.parse_args().file) as f:
-        print(
-            f.read()
-            .replace("\a", "\\a")
-            .replace("\b", "\\b")
-            .replace("\f", "\\f")
-            .replace("\n", "\\n")
-            .replace("\r", "\\r")
-            .replace("\t", "\\t")
-            .replace("\v", "\\v")
-        )
+        for line in f.readlines():
+            print(
+                (
+                    "{}".format(line)
+                    .replace("\a", "\\a\a")
+                    .replace("\b", "\\b\b")
+                    .replace("\f", "\\f\f")
+                    .replace("\n", "\\n\n")
+                    .replace("\r", "\\r\r")
+                    .replace("\t", "\\t\t")
+                    .replace("\v", "\\v\v")
+                    .replace("\N{NO-BREAK SPACE}", "␣")
+                ),
+                end="",
+            )
